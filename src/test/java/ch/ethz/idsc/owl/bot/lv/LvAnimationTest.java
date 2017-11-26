e59d796377635c24695c0ffcaa06b16274489878
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-11-26 18:27:57 +0100

    edit tests

--- src/test/java/ch/ethz/idsc/owl/bot/lv/LvAnimationTest.java
+++ src/test/java/ch/ethz/idsc/owl/bot/lv/LvAnimationTest.java
@@ -4,14 +4,12 @@ package ch.ethz.idsc.owl.bot.lv;
 import java.util.Collection;
 
 import ch.ethz.idsc.owl.gui.ren.VectorFieldRender;
-import ch.ethz.idsc.owl.gui.win.OwlyAnimationFrame;
 import ch.ethz.idsc.owl.math.StateSpaceModel;
 import ch.ethz.idsc.owl.math.VectorFields;
 import ch.ethz.idsc.owl.math.flow.Flow;
 import ch.ethz.idsc.owl.math.sample.BoxRandomSample;
 import ch.ethz.idsc.owl.math.sample.RandomSample;
 import ch.ethz.idsc.owl.math.sample.RandomSampleInterface;
-import ch.ethz.idsc.owl.math.state.EmptyTrajectoryRegionQuery;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
@@ -19,18 +17,14 @@ import junit.framework.TestCase;
 
 public class LvAnimationTest extends TestCase {
   public void testSimple() {
-    OwlyAnimationFrame owlyAnimationFrame = new OwlyAnimationFrame();
     StateSpaceModel stateSpaceModel = LvStateSpaceModel.of(1, 2);
     Collection<Flow> controls = LvControls.create(stateSpaceModel, 2);
-    owlyAnimationFrame.set(new LvEntity(stateSpaceModel, Tensors.vector(2, 0.3), controls));
-    owlyAnimationFrame.setObstacleQuery(EmptyTrajectoryRegionQuery.INSTANCE);
-    // ---
+    new LvEntity(stateSpaceModel, Tensors.vector(2, 0.3), controls);
     Tensor range = Tensors.vector(6, 5);
     VectorFieldRender vectorFieldRender = new VectorFieldRender();
     RandomSampleInterface sampler = new BoxRandomSample(Tensors.vector(0, 0), range);
     Tensor points = Tensor.of(RandomSample.of(sampler, 1000).stream());
     vectorFieldRender.uv_pairs = //
         VectorFields.of(stateSpaceModel, points, LvEntity.FALLBACK_CONTROL, RealScalar.of(0.04));
-    owlyAnimationFrame.addBackground(vectorFieldRender);
   }
 }
