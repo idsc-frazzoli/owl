227f5a9ab7f6b8003cddde3ac997170729856beb
Jan Hakenberg <jan.hakenberg@gmail.com>
2017-12-06 10:59:28 +0100

    tests, cleanup

---
+++ src/test/java/ch/ethz/idsc/owl/glc/core/TrajectoryPlannerTest.java
@@ -0,0 +1,10 @@
+// code by jph
+package ch.ethz.idsc.owl.glc.core;
+
+import junit.framework.TestCase;
+
+public class TrajectoryPlannerTest extends TestCase {
+  public void testSimple() {
+    // ---
+  }
+}
