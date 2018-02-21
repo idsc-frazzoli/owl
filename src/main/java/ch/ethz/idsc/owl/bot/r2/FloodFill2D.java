319ef00b2c4041e6cbd234154a8012abdba5dbe6
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-02-21 22:49:05 +0100

    flood fill 2d int ttl

--- src/main/java/ch/ethz/idsc/owl/bot/r2/FloodFill2D.java
+++ src/main/java/ch/ethz/idsc/owl/bot/r2/FloodFill2D.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
@@ -16,19 +17,21 @@ import ch.ethz.idsc.tensor.sca.Decrement;
 /** computes manhatten distance by flood fill */
 public class FloodFill2D {
   /** @param image
-   * @param ttl
+   * @param ttl non-negative
    * @param seeds
    * @return distance in exact precision */
-  public static Tensor of(Tensor image, Scalar ttl, Set<Tensor> seeds) {
-    return new FloodFill2D(image, ttl, seeds).array;
+  public static Tensor of(Tensor image, int ttl, Set<Tensor> seeds) {
+    if (ttl < 0)
+      throw new RuntimeException("ttl=" + ttl);
+    return new FloodFill2D(image, RealScalar.of(ttl), seeds).array;
   }
 
   /** seeds are generated from given tensor using {@link #seeds(Tensor)}
    * 
    * @param image
-   * @param ttl
+   * @param ttl non-negative
    * @return distance in exact precision */
-  public static Tensor of(Tensor image, Scalar ttl) {
+  public static Tensor of(Tensor image, int ttl) {
     return of(image, ttl, seeds(image));
   }
 
--- src/test/java/ch/ethz/idsc/owl/bot/r2/FloodFill2DTest.java
+++ src/test/java/ch/ethz/idsc/owl/bot/r2/FloodFill2DTest.java
@@ -5,8 +5,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import ch.ethz.idsc.tensor.ExactScalarQ;
-import ch.ethz.idsc.tensor.RealScalar;
-import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.alg.Array;
@@ -16,7 +14,7 @@ public class FloodFill2DTest extends TestCase {
   public void testSimple() {
     Tensor tensor = Array.zeros(5, 6);
     Tensor seeds = Tensors.fromString("{{2,2},{4,3}}");
-    Scalar ttl = RealScalar.of(3);
+    int ttl = 3;
     Tensor manh = FloodFill2D.of(tensor, ttl, seeds.stream().collect(Collectors.toSet()));
     String s = "{{0, 0, 1, 0, 0, 0}, {0, 1, 2, 1, 0, 0}, {1, 2, 3, 2, 1, 0}, {0, 1, 2, 2, 1, 0}, {0, 1, 2, 3, 2, 1}}";
     assertEquals(manh, Tensors.fromString(s));
@@ -27,7 +25,7 @@ public class FloodFill2DTest extends TestCase {
     Tensor tensor = Array.zeros(5, 6);
     tensor.set(Tensors.vector(0, 1, 1, 1, 1), Tensor.ALL, 1);
     Tensor seeds = Tensors.fromString("{{2,2},{4,3}}");
-    Scalar ttl = RealScalar.of(10);
+    int ttl = 10;
     Tensor manh = FloodFill2D.of(tensor, ttl, seeds.stream().collect(Collectors.toSet()));
     String s = "{{6, 7, 8, 7, 6, 5}, {5, 0, 9, 8, 7, 6}, {4, 0, 10, 9, 8, 7}, {3, 0, 9, 9, 8, 7}, {2, 0, 9, 10, 9, 8}}";
     assertEquals(manh, Tensors.fromString(s));
@@ -48,12 +46,23 @@ public class FloodFill2DTest extends TestCase {
     assertTrue(seeds.contains(Tensors.vector(0, 4)));
     assertTrue(seeds.contains(Tensors.vector(1, 2)));
     // ---
-    Scalar ttl = RealScalar.of(3);
+    int ttl = 3;
     Tensor manh = FloodFill2D.of(tensor, ttl, seeds);
     String s = "{{1, 1, 2, 3, 3}, {2, 2, 3, 0, 0}, {3, 2, 2, 3, 0}, {0, 3, 2, 2, 3}}";
     assertEquals(manh, Tensors.fromString(s));
     assertTrue(ExactScalarQ.all(manh));
     // System.out.println(Pretty.of(manh));
     // System.out.println(manh);
+  }
+
+  public void testInvalid() {
+    Tensor tensor = Array.zeros(5, 6);
+    int ttl = -1;
+    try {
+      FloodFill2D.of(tensor, ttl);
+      assertTrue(false);
+    } catch (Exception exception) {
+      // ---
+    }
   }
 }
