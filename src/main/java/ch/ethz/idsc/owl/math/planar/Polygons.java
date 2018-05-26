f6b9ce820f2ccf60f08670d97b644720b571c9f4
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-05-26 15:39:38 +0200

    polygon area by @ValentinaCavinato

--- src/main/java/ch/ethz/idsc/owl/math/planar/Polygons.java
+++ src/main/java/ch/ethz/idsc/owl/math/planar/Polygons.java
@@ -1,7 +1,7 @@
-// code by jph
-// adapted from PNPOLY - Point Inclusion in Polygon Test W. Randolph Franklin (WRF)
+// code by jph and vc
 package ch.ethz.idsc.owl.math.planar;
 
+import ch.ethz.idsc.tensor.RationalScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
@@ -12,6 +12,7 @@ public enum Polygons {
    * @param point of which only the first two coordinates will be considered
    * @return true, if point is inside polygon, otherwise false
    * @throws Exception if the first two entries of point are not of type {@link Scalar} */
+  // adapted from PNPOLY - Point Inclusion in Polygon Test W. Randolph Franklin (WRF)
   public static boolean isInside(Tensor polygon, Tensor point) {
     final Scalar tx = point.Get(0);
     final Scalar ty = point.Get(1);
@@ -32,5 +33,22 @@ public enum Polygons {
       }
     }
     return c;
+  }
+
+  /** @param polygon not necessarily convex
+   * @return signed area circumscribed by given polygon,
+   * area is positive when polygon is in counter-clockwise direction */
+  // inspired by https://www.mathopenref.com/coordpolygonarea.html
+  public static Scalar area(Tensor polygon) {
+    int last = polygon.length() - 1;
+    Scalar sum = det(polygon.get(last), polygon.get(0));
+    for (int index = 0; index < last; ++index)
+      sum = sum.add(det(polygon.get(index), polygon.get(index + 1)));
+    return sum.multiply(RationalScalar.HALF);
+  }
+
+  // helper function
+  private static Scalar det(Tensor p, Tensor q) {
+    return Cross2D.of(p).dot(q).Get();
   }
 }
