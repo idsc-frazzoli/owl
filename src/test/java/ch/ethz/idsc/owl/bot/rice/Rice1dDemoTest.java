ed08195b436af706681d33015eea1da51774841e
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-05-27 19:54:09 +0200

    tests

--- src/test/java/ch/ethz/idsc/owl/bot/rice/Rice1dDemoTest.java
+++ src/test/java/ch/ethz/idsc/owl/bot/rice/Rice1dDemoTest.java
@@ -4,7 +4,7 @@ package ch.ethz.idsc.owl.bot.rice;
 import ch.ethz.idsc.owl.glc.core.TrajectoryPlanner;
 import junit.framework.TestCase;
 
-public class RiceD1DemoTest extends TestCase {
+public class Rice1dDemoTest extends TestCase {
   public void testFindGoal() {
     TrajectoryPlanner trajectoryPlanner = Rice1dDemo.simple();
     assertTrue(trajectoryPlanner.getBest().isPresent());
