df9645a0e0ced844438a2112d279fa4333a1f5c9
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-06-15 06:09:35 +0200

    sparse image cost function

---
+++ src/main/java/ch/ethz/idsc/owl/bot/r2/SparseImageCostFunction.java
@@ -0,0 +1,24 @@
+// code by ynager
+package ch.ethz.idsc.owl.bot.r2;
+
+import java.util.List;
+
+import ch.ethz.idsc.owl.data.Lists;
+import ch.ethz.idsc.owl.glc.core.GlcNode;
+import ch.ethz.idsc.owl.math.flow.Flow;
+import ch.ethz.idsc.owl.math.state.StateTime;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Tensor;
+
+/** slightly different from {@link ImageCostFunction}
+ * because evaluation only happens at last state of trajectory */
+public class SparseImageCostFunction extends ImageCostFunction {
+  public SparseImageCostFunction(Tensor image, Tensor range, Scalar outside) {
+    super(image, range, outside);
+  }
+
+  @Override // from CostIncrementFunction
+  public Scalar costIncrement(GlcNode glcNode, List<StateTime> trajectory, Flow flow) {
+    return flipYXTensorInterp.at(Lists.getLast(trajectory).state());
+  }
+}
