c33dc12e95293bb0e428052c509b9811cd1d3578
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-09-07 16:55:18 +0200

    tse2 fallback control

---
+++ src/main/java/ch/ethz/idsc/owl/bot/tse2/Tse2FallbackControl.java
@@ -0,0 +1,33 @@
+// code by jph
+package ch.ethz.idsc.owl.bot.tse2;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import ch.ethz.idsc.owl.math.state.EntityControl;
+import ch.ethz.idsc.owl.math.state.ProviderRank;
+import ch.ethz.idsc.owl.math.state.StateTime;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.sca.Sign;
+
+public class Tse2FallbackControl implements EntityControl, Serializable {
+  private final Scalar magnitude;
+
+  public Tse2FallbackControl(Scalar magnitude) {
+    this.magnitude = magnitude;
+  }
+
+  @Override
+  public Optional<Tensor> control(StateTime tail, Scalar now) {
+    Scalar vx = tail.state().Get(Tse2StateSpaceModel.STATE_INDEX_VEL);
+    return Optional.of(Tensors.of(RealScalar.ZERO, Sign.FUNCTION.apply(vx).multiply(magnitude).negate()));
+  }
+
+  @Override
+  public ProviderRank getProviderRank() {
+    return ProviderRank.FALLBACK;
+  }
+}
