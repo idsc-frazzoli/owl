ead60aa031be240526ac8bb1e26a34cf70c4dbb3
André Stoll <astoll@student.ethz.ch>
2018-11-30 17:17:59 +0100

    State Space Model and according test for hot air balloon

---
+++ src/main/java/ch/ethz/idsc/owl/balloon/BalloonStateSpaceModel.java
@@ -0,0 +1,42 @@
+// code by astoll
+package ch.ethz.idsc.owl.balloon;
+
+import ch.ethz.idsc.owl.math.StateSpaceModel;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.qty.Quantity;
+
+/** state space model taken from ??? Chapter 2.7.2 by ???
+ * 
+ * @param x = {height [m], vertival velocity [m * s^-1], incremental air temperature (theta) [K]}
+ * @param u = proportional of heat delivered to air mass by the burner [K * s^-1]
+ * @author Andre */
+/* package */ enum BalloonStateSpaceModel implements StateSpaceModel {
+  INSTANCE;
+  /* constants of the state space model */
+  private static final Scalar TAU1 = Quantity.of(1, "s");
+  private static final Scalar TAU2 = Quantity.of(2, "s");
+  private static final Scalar SIGMA = Quantity.of(1, "m * K^-1 * s^-2");
+  /* unknown perturbation due to vertical velocity of wind (assumed as -2 for now) */
+  private Scalar w = Quantity.of(-2, "m * s^-1");
+
+  @Override
+  public Tensor f(Tensor x, Tensor u) {
+    // y' = vel
+    // vel' = (-1 / tau1) * vel + sigma * theta + w / tau2
+    // theta' = - theta / tau1 + u
+    // ---
+    // Scalar y = x.Get(0);
+    Scalar vel = x.Get(1);
+    Scalar theta = x.Get(2);
+    return Tensors.of(vel, //
+        vel.negate().divide(TAU2).add(theta.multiply(SIGMA)).add(w.divide(TAU2)), //
+        theta.negate().divide(TAU1).add(u.Get(0)));
+  }
+
+  @Override
+  public Scalar getLipschitz() {
+    throw new UnsupportedOperationException();
+  }
+}
---
+++ src/test/java/ch/ethz/idsc/owl/balloon/BalloonStateSpaceModelTest.java
@@ -0,0 +1,16 @@
+package ch.ethz.idsc.owl.balloon;
+
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import junit.framework.TestCase;
+
+public class BalloonStateSpaceModelTest extends TestCase {
+  Tensor x = Tensors.fromString("{2[m],2[m*s^-1],4[K]}");
+  Tensor u = Tensors.fromString("{3[K*s^-1]}");
+  
+  
+  public void testValidity() {
+    Tensor expected = Tensors.fromString("{2[m*s^-1],2[m*s^-2],-1[K*s^-1]}");
+    assertEquals(expected, BalloonStateSpaceModel.INSTANCE.f(x, u));
+  }
+}
