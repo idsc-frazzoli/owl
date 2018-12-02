444ea6f39675333b389ed054ff8526b474fce995
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-12-02 06:24:34 +0100

    configurable parameters

--- src/main/java/ch/ethz/idsc/owl/balloon/BalloonStateSpaceModel.java
+++ src/main/java/ch/ethz/idsc/owl/balloon/BalloonStateSpaceModel.java
@@ -5,21 +5,32 @@ import ch.ethz.idsc.owl.math.StateSpaceModel;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
-import ch.ethz.idsc.tensor.qty.Quantity;
 
-/** state space model taken from ? Chapter 2.7.2 by ?
+/** state space model taken from the book
+ * "Differentially Flat Systems" Chapter 2.7.2
+ * by Hebertt Sira-Ramirez, Sunil K. Agrawal
  * 
- * @param x = {height [m], vertival velocity [m * s^-1], incremental air temperature (theta) [K]}
+ * @param x = {height [m], vertical velocity [m * s^-1], incremental air temperature (theta) [K]}
  * @param u = proportional of heat delivered to air mass by the burner [K * s^-1]
  * @author Andre */
-/* package */ enum BalloonStateSpaceModel implements StateSpaceModel {
-  INSTANCE;
-  /* constants of the state space model */
-  private static final Scalar TAU1 = Quantity.of(1, "s");
-  private static final Scalar TAU2 = Quantity.of(2, "s");
-  private static final Scalar SIGMA = Quantity.of(1, "m * K^-1 * s^-2");
-  /* unknown perturbation due to vertical velocity of wind (assumed as -2 for now) */
-  private Scalar w = Quantity.of(-2, "m * s^-1");
+/* package */ class BalloonStateSpaceModel implements StateSpaceModel {
+  /** constants of the state space model */
+  private final Scalar tau1;
+  private final Scalar tau2;
+  private final Scalar sigma;
+  /** unknown perturbation due to vertical velocity of wind (assumed as -2 for now) */
+  private final Scalar w;
+
+  /** @param tau1
+   * @param tau2
+   * @param sigma
+   * @param w */
+  public BalloonStateSpaceModel(Scalar tau1, Scalar tau2, Scalar sigma, Scalar w) {
+    this.tau1 = tau1;
+    this.tau2 = tau2;
+    this.sigma = sigma;
+    this.w = w;
+  }
 
   @Override
   public Tensor f(Tensor x, Tensor u) {
@@ -27,12 +38,13 @@ import ch.ethz.idsc.tensor.qty.Quantity;
     // vel' = (-1 / tau1) * vel + sigma * theta + w / tau2
     // theta' = - theta / tau1 + u
     // ---
-    // Scalar y = x.Get(0);
+    // Scalar y = x.Get(0); // altitude
     Scalar vel = x.Get(1);
     Scalar theta = x.Get(2);
-    return Tensors.of(vel, //
-        vel.negate().divide(TAU2).add(theta.multiply(SIGMA)).add(w.divide(TAU2)), //
-        theta.negate().divide(TAU1).add(u.Get(0)));
+    return Tensors.of( //
+        vel, //
+        vel.negate().divide(tau2).add(theta.multiply(sigma)).add(w.divide(tau2)), //
+        theta.negate().divide(tau1).add(u.Get(0)));
   }
 
   @Override
