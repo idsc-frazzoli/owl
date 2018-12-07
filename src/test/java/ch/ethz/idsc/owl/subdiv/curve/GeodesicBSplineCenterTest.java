29f8d96dbf04699f74cd672a3203b3436ec51593
Jan Hakenberg <jan.hakenberg@gmail.com>
2018-12-07 14:44:55 +0100

    geodesic bspline center

---
+++ src/test/java/ch/ethz/idsc/owl/subdiv/curve/GeodesicBSplineCenterTest.java
@@ -0,0 +1,35 @@
+// code by jph
+package ch.ethz.idsc.owl.subdiv.curve;
+
+import ch.ethz.idsc.owl.math.group.RnGeodesic;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import junit.framework.TestCase;
+
+public class GeodesicBSplineCenterTest extends TestCase {
+  public void testSimple3() {
+    GeodesicBSplineCenter geodesicBSplineCenter = //
+        new GeodesicBSplineCenter(RnGeodesic.INSTANCE);
+    Tensor tensor = geodesicBSplineCenter.apply(Tensors.vector(1, 2, 3));
+    assertEquals(tensor, RealScalar.of(2));
+  }
+
+  public void testSimple5() {
+    GeodesicBSplineCenter geodesicBSplineCenter = //
+        new GeodesicBSplineCenter(RnGeodesic.INSTANCE);
+    Tensor tensor = geodesicBSplineCenter.apply(Tensors.vector(1, 2, 3, 4, 5));
+    assertEquals(tensor, RealScalar.of(3));
+  }
+
+  public void testFail() {
+    GeodesicBSplineCenter geodesicBSplineCenter = //
+        new GeodesicBSplineCenter(RnGeodesic.INSTANCE);
+    try {
+      geodesicBSplineCenter.apply(Tensors.vector(1, 2, 3, 4));
+      fail();
+    } catch (Exception exception) {
+      // ---
+    }
+  }
+}
