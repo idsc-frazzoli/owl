57c035b581d26c3af4078ae1ba309781b8a13a5d
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-01-06 14:09:34 +0100

    universal geodesic demo

--- src/test/java/ch/ethz/idsc/sophus/app/misc/GeodesicDemoTest.java
+++ src/test/java/ch/ethz/idsc/sophus/app/misc/GeodesicDemoTest.java
@@ -4,8 +4,8 @@ package ch.ethz.idsc.sophus.app.misc;
 import ch.ethz.idsc.sophus.app.api.AbstractDemoHelper;
 import junit.framework.TestCase;
 
-public class H2GeodesicDemoTest extends TestCase {
+public class GeodesicDemoTest extends TestCase {
   public void testSimple() {
-    AbstractDemoHelper.brief(new H2GeodesicDemo());
+    AbstractDemoHelper.brief(new GeodesicDemo());
   }
 }
