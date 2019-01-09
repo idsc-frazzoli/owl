c1da8f756bdebb0e80ab980afa9a59d600373a50
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-01-09 14:46:52 +0100

    minor edits in package sophus app util

--- src/main/java/ch/ethz/idsc/sophus/app/util/LazyMouseListener.java
+++ src/main/java/ch/ethz/idsc/sophus/app/util/LazyMouseListener.java
@@ -3,7 +3,7 @@ package ch.ethz.idsc.sophus.app.util;
 
 import java.awt.event.MouseEvent;
 
-interface LazyMouseListener {
+public interface LazyMouseListener {
   void lazyClicked(MouseEvent myMouseEvent);
 
   default void lazyDragged(MouseEvent myMouseEvent) {
