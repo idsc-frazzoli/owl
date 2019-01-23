0b328aea5d100be62186b14b728a68d5569354a6
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-01-23 19:22:45 +0100

    minor edits, tensor v068

--- src/test/java/ch/ethz/idsc/sophus/filter/StaticHelperCausalTest.java
+++ src/test/java/ch/ethz/idsc/sophus/filter/StaticHelperCausalTest.java
@@ -1,8 +1,10 @@
 //code by ob
 package ch.ethz.idsc.sophus.filter;
 
+import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.mat.HilbertMatrix;
 import ch.ethz.idsc.tensor.sca.Chop;
 import junit.framework.TestCase;
 
@@ -11,5 +13,32 @@ public class StaticHelperCausalTest extends TestCase {
     Tensor mask = Tensors.vector(.2, .4, .9, .5, .5);
     Tensor correct = Tensors.vector(0.012, 0.003, 0.01, 0.225, 0.25, 0.5);
     Chop._12.requireClose(correct, StaticHelperCausal.splits(mask));
+  }
+
+  public void testScalarFail() {
+    try {
+      StaticHelperCausal.splits(RealScalar.ONE);
+      fail();
+    } catch (Exception exception) {
+      // ---
+    }
+  }
+
+  public void testMatrixFail() {
+    try {
+      StaticHelperCausal.splits(HilbertMatrix.of(2, 3));
+      fail();
+    } catch (Exception exception) {
+      // ---
+    }
+  }
+
+  public void testEmptyFail() {
+    try {
+      StaticHelperCausal.splits(Tensors.empty());
+      fail();
+    } catch (Exception exception) {
+      // ---
+    }
   }
 }
