2de40bd519444c8778e261a5d115b35c91619f93
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-01-30 12:10:11 +0100

    misc simplifications

--- src/main/java/ch/ethz/idsc/sophus/math/WindowCenterSampler.java
+++ src/main/java/ch/ethz/idsc/sophus/math/WindowCenterSampler.java
@@ -3,7 +3,6 @@ package ch.ethz.idsc.sophus.math;
 
 import ch.ethz.idsc.tensor.RationalScalar;
 import ch.ethz.idsc.tensor.Tensor;
-import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.alg.Subdivide;
 import ch.ethz.idsc.tensor.sca.ScalarUnaryOperator;
 
@@ -13,16 +12,13 @@ public class WindowCenterSampler extends WindowBaseSampler {
     super(windowFunction);
   }
 
-  @Override // from IntegerTensorFunction
-  public Tensor apply(Integer extent) {
-    if (extent == 0)
-      return Tensors.vector(1);
-    Tensor vector = isContinuous //
+  @Override // from WindowBaseSampler
+  protected Tensor samples(int extent) {
+    return isContinuous //
         ? Subdivide.of(RationalScalar.HALF.negate(), RationalScalar.HALF, 2 * extent + 2) //
             .map(windowFunction) //
             .extract(1, 2 * extent + 2)
         : Subdivide.of(RationalScalar.HALF.negate(), RationalScalar.HALF, 2 * extent) //
             .map(windowFunction);
-    return NORMALIZE.apply(vector);
   }
 }
