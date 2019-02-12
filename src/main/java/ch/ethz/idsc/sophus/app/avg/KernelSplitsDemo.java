6a14db95a28be785a039ce0c973e73bf9b406273
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-02-12 15:21:49 +0100

    extrapolation split demo, kernel split demo

---
+++ src/main/java/ch/ethz/idsc/sophus/app/avg/KernelSplitsDemo.java
@@ -0,0 +1,18 @@
+// code by jph
+package ch.ethz.idsc.sophus.app.avg;
+
+import java.awt.Dimension;
+import java.util.Arrays;
+
+import ch.ethz.idsc.sophus.app.util.SpinnerLabel;
+import ch.ethz.idsc.sophus.math.SmoothingKernel;
+
+/* package */ abstract class KernelSplitsDemo extends GeodesicSplitsDemo {
+  final SpinnerLabel<SmoothingKernel> spinnerKernel = new SpinnerLabel<>();
+
+  public KernelSplitsDemo() {
+    spinnerKernel.setList(Arrays.asList(SmoothingKernel.values()));
+    spinnerKernel.setValue(SmoothingKernel.GAUSSIAN);
+    spinnerKernel.addToComponentReduced(timerFrame.jToolBar, new Dimension(180, 28), "filter");
+  }
+}
