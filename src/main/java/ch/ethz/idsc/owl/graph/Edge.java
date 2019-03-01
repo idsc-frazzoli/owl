b9b7ffa38dd2db2ef886e81350c9af091b8be2bd
André Stoll <astoll@student.ethz.ch>
2019-03-01 19:38:56 +0100

    Digraph, Edge (WeightedEdge) and Vertex

---
+++ src/main/java/ch/ethz/idsc/owl/graph/Edge.java
@@ -0,0 +1,20 @@
+// code by astoll
+package ch.ethz.idsc.owl.graph;
+
+public class Edge<T> {
+  public final Vertex<T> source;
+  public final Vertex<T> target;
+
+  public Edge(Vertex<T> source, Vertex<T> target) {
+    this.source = source;
+    this.target = target;
+  }
+
+  public Vertex<T> getSource() {
+    return this.source;
+  }
+
+  public Vertex<T> getTarget() {
+    return this.target;
+  }
+}
