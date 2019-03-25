19ec0d3f042d5fe82db292573fe180c19223da9f
rot8 <oliverbr@ethz.ch>
2019-03-25 19:08:44 +0100

    Minor enhancements

---
+++ src/main/java/ch/ethz/idsc/sophus/curve/GeodesicCatmullRom.java
@@ -0,0 +1,78 @@
+// code by ob
+// adapted from Schafer 2006
+package ch.ethz.idsc.sophus.curve;
+
+import java.util.Objects;
+
+import ch.ethz.idsc.sophus.math.GeodesicInterface;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.TensorRuntimeException;
+import ch.ethz.idsc.tensor.alg.VectorQ;
+import ch.ethz.idsc.tensor.opt.ScalarTensorFunction;
+
+/** CatmullRom denotes the function that is defined
+ * by control points over a sequence of knots. */
+public class GeodesicCatmullRom implements ScalarTensorFunction {
+  // TODO OB: correct input lengths
+  /** @param geodesicInterface non null
+   * @param knots vector of length degree * 2
+   * @param control points of length degree + 1
+   * @return
+   * @throws Exception if given knots is not a vector */
+  public static GeodesicCatmullRom of(GeodesicInterface geodesicInterface, Tensor knots, Tensor control) {
+    int degree = knots.length() / 2;
+    if (control.length() != degree + 1)
+      throw TensorRuntimeException.of(knots, control);
+    return new GeodesicCatmullRom(Objects.requireNonNull(geodesicInterface), degree, VectorQ.require(knots), control);
+  }
+
+  // ---
+  private final GeodesicInterface geodesicInterface;
+  private final int degree;
+  private final Tensor knots;
+  private final Tensor control;
+
+  // TODO OB: correct input lengths
+  /** @param degree
+   * @param knots vector of length degree * 2
+   * @param control points of length degree + 1 */
+  /* package */ GeodesicCatmullRom(GeodesicInterface geodesicInterface, int degree, Tensor knots, Tensor control) {
+    this.geodesicInterface = geodesicInterface;
+    this.degree = degree;
+    this.knots = knots;
+    this.control = control;
+  }
+
+  // TODO OB: compare with schafer and implement
+  @Override
+  public Tensor apply(Scalar x) {
+    Tensor d = control.copy(); // d is modified over the course of the algorithm
+    for (int r = 1; r < degree + 1; ++r)
+      for (int j = degree; j >= r; --j) {
+        Scalar kj1 = knots.Get(j - 1);
+        Scalar num = x.subtract(kj1);
+        Scalar den = knots.Get(j + degree - r).subtract(kj1);
+        Scalar alpha = Scalars.isZero(den) //
+            ? RealScalar.ZERO
+            : num.divide(den);
+        Tensor a0 = d.get(j - 1);
+        d.set(dj -> geodesicInterface.split(a0, dj, alpha), j);
+      }
+    return d.get(degree);
+  }
+
+  public int degree() {
+    return degree;
+  }
+
+  public Tensor control() {
+    return control.unmodifiable();
+  }
+
+  public Tensor knots() {
+    return knots.unmodifiable();
+  }
+}
--- src/main/java/ch/ethz/idsc/sophus/sym/SymWeightsToSplits.java
+++ src/main/java/ch/ethz/idsc/sophus/sym/SymWeightsToSplits.java
@@ -4,27 +4,51 @@ package ch.ethz.idsc.sophus.sym;
 import ch.ethz.idsc.tensor.RationalScalar;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
-import ch.ethz.idsc.tensor.red.Total;
+import ch.ethz.idsc.tensor.red.Norm;
 
 /* package */ class SymWeightsToSplits {
-  static Tensor split(Tensor pL, Tensor pR) {
-    return Total.of(pR).divide((Scalar) Total.of(pL).add(Total.of(pR)));
-  }
+  public static Scalar leftWeights = RealScalar.ZERO;
+  public static Scalar rightWeights = RealScalar.ZERO;
+  public static Tensor result = Tensors.empty();
 
-  static Tensor conversion(Tensor weights) {
-    // I'm not sure about the inputs I need
-    // Either: Weight mask + tree shape
-    // or: tree shape and kernel
-    return Tensors.empty();
+  static Scalar split(Scalar pL, Scalar pR) {
+    return pR.divide(pL.add(pR));
+  }
+//TODO OB: either work with symLinks => JH, or correct the calculation of weights
+  static Tensor recursion(Tensor tree, Tensor weights) {
+    if (tree.get(0).length() == -1 && tree.get(1).length() == -1) {
+      Scalar pL = weights.Get(Scalars.intValueExact(tree.Get(0)));
+      Scalar pR = weights.Get(Scalars.intValueExact(tree.Get(1)));
+      leftWeights = pL.add(pR);
+      rightWeights = pL.add(pR);
+      return Tensors.of(pL, pR, split(pL, pR));
+    } else if (tree.get(0).length() == -1) {
+      // make recursion to the right branch of the tree
+      // calculate rightWeights
+      leftWeights = weights.Get(Scalars.intValueExact(tree.Get(0)));
+      return Tensors.of(tree.get(0), recursion(tree.get(1), weights), split(leftWeights, rightWeights));
+    } else if (tree.get(1).length() == -1) {
+      // make recursion to the left branch of the tree
+      // calculate leftweights
+      rightWeights = weights.Get(Scalars.intValueExact(tree.Get(1)));
+      return Tensors.of(recursion(tree.get(0), weights), tree.get(1), split(leftWeights, rightWeights));
+    } else {
+      // take leftweights and rightweights
+      return Tensors.of(recursion(tree.get(0), weights), recursion(tree.get(1), weights), split(leftWeights, rightWeights));
+    }
   }
 
   public static void main(String[] args) {
-    // inputs
-    Tensor weights = Tensors.vector(.2, .2, .2, .2, .2);
+    // Test inputs
+    Tensor weights = Tensors.vector(0, 1, 2, 3, 4);
     Tensor tree = Tensors.of(Tensors.vector(0, 1), Tensors.of(Tensors.vector(2, 3), RealScalar.of(4)));
-    // output:
-    Tensor output = Tensors.of(Tensors.vector(0, 1, .5), Tensors.of(Tensors.vector(2, 3, .5), RealScalar.of(4), RationalScalar.of(1, 3)), RealScalar.of(0.6));
+    // Test output:
+    Tensor output = Tensors.of(Tensors.vector(0, 1, 1), Tensors.of(Tensors.vector(2, 3, 3.0 / 5.0), RealScalar.of(4), RationalScalar.of(4, 9)),
+        RealScalar.of(0.9));
+    System.out.println(recursion(tree, weights));
+    System.err.println(output);
   }
 }
