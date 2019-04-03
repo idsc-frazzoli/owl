c82ae26d53412425a06c3ad14810103855f0ac02
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-04-03 05:52:21 +0200

    se2 parametric distance tests with units

--- src/test/java/ch/ethz/idsc/sophus/group/Se2ParametricDistanceTest.java
+++ src/test/java/ch/ethz/idsc/sophus/group/Se2ParametricDistanceTest.java
@@ -4,6 +4,7 @@ package ch.ethz.idsc.sophus.group;
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.qty.Quantity;
 import ch.ethz.idsc.tensor.sca.Chop;
 import junit.framework.TestCase;
 
@@ -16,5 +17,15 @@ public class Se2ParametricDistanceTest extends TestCase {
   public void testHalf() {
     Scalar scalar = Se2ParametricDistance.of(Tensors.vector(1, 2, 3), Tensors.vector(1, 2 + 2 * 3, 3 + Math.PI));
     Chop._14.requireClose(scalar, RealScalar.of(3 * Math.PI));
+  }
+
+  public void testSimpleUnits() {
+    Scalar scalar = Se2ParametricDistance.of(Tensors.fromString("{1[m], 2[m], 3}"), Tensors.fromString("{4[m], 6[m], 3}"));
+    assertEquals(scalar, Quantity.of(5, "m"));
+  }
+
+  public void testOtherUnits() {
+    Scalar scalar = Se2ParametricDistance.of(Tensors.fromString("{1[m], 2[m], 3}"), Tensors.fromString("{4[m], 6[m], 3.3}"));
+    Chop._12.close(scalar, Quantity.of(5.018799335788676, "m"));
   }
 }
