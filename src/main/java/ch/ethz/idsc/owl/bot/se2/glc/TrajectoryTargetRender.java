b0c73abcb162358684c49831e6255b4521b03396
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-04-05 00:53:13 +0200

    signed curvature 2d convention thanks to @JoelGaechter

--- src/main/java/ch/ethz/idsc/owl/bot/se2/glc/TrajectoryTargetRender.java
+++ src/main/java/ch/ethz/idsc/owl/bot/se2/glc/TrajectoryTargetRender.java
@@ -1,9 +1,9 @@
 package ch.ethz.idsc.owl.bot.se2.glc;
 
-import ch.ethz.idsc.owl.gui.win.GeometricLayer;
-
 import java.awt.Shape;
 import java.util.Optional;
+
+import ch.ethz.idsc.owl.gui.win.GeometricLayer;
 
 /* package */ interface TrajectoryTargetRender {
   Optional<Shape> toTarget(GeometricLayer geometricLayer);
