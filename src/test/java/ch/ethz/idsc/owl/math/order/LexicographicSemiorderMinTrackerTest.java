10b51251e7cf09a8e4c7c200cef21d915da02f07
André Stoll <astoll@ethz.ch>
2019-04-08 13:01:50 +0200

    updateFeasibleInputs in LexicographicSemiorderTest

--- src/test/java/ch/ethz/idsc/owl/math/order/LexicographicSemiorderMinTrackerTest.java
+++ src/test/java/ch/ethz/idsc/owl/math/order/LexicographicSemiorderMinTrackerTest.java
@@ -6,31 +6,30 @@ import ch.ethz.idsc.tensor.Tensors;
 import junit.framework.TestCase;
 
 public class LexicographicSemiorderMinTrackerTest extends TestCase {
-  public void testDigestSimple() {
-    Tensor slackVector = Tensors.fromString("{1,1,1}");
-    LexicographicSemiorderMinTracker LSMT1 = LexicographicSemiorderMinTracker.withList(slackVector);
-    LexicographicSemiorderMinTracker LSMT2 = LexicographicSemiorderMinTracker.withSet(slackVector);
-    Tensor x = Tensors.fromString("{1,2,2}");
-    LSMT1.digest(x);
-    LSMT1.digest(x);
-    LSMT2.digest(x);
-    LSMT2.digest(x);
-    assertTrue(!LSMT1.getFeasibleInputs().isEmpty() && LSMT1.getFeasibleInputs().size() == 1);
-    assertTrue(!LSMT2.getFeasibleInputs().isEmpty() && LSMT2.getFeasibleInputs().size() == 1);
-  }
-
-  public void testDigestFalseDim() {
-    Tensor slackVector = Tensors.fromString("{1,1,1}");
-    LexicographicSemiorderMinTracker LSMT1 = LexicographicSemiorderMinTracker.withList(slackVector);
-    Tensor x = Tensors.fromString("{1,2,2,3}");
-    try {
-      LSMT1.digest(x);
-      fail();
-    } catch (Exception exception) {
-      // ---
-    }
-  }
-
+   public void testDigestSimple() {
+   Tensor slackVector = Tensors.fromString("{1,1,1}");
+   LexicographicSemiorderMinTracker LSMT1 = LexicographicSemiorderMinTracker.withList(slackVector);
+   LexicographicSemiorderMinTracker LSMT2 = LexicographicSemiorderMinTracker.withSet(slackVector);
+   Tensor x = Tensors.fromString("{1,2,2}");
+   LSMT1.digest(x);
+   LSMT1.digest(x);
+   LSMT2.digest(x);
+   LSMT2.digest(x);
+   assertTrue(!LSMT1.getFeasibleInputs().isEmpty() && LSMT1.getFeasibleInputs().size() > 1);
+   assertTrue(!LSMT2.getFeasibleInputs().isEmpty() && LSMT2.getFeasibleInputs().size() == 1);
+   }
+  
+   public void testDigestFalseDim() {
+   Tensor slackVector = Tensors.fromString("{1,1,1}");
+   LexicographicSemiorderMinTracker LSMT1 = LexicographicSemiorderMinTracker.withList(slackVector);
+   Tensor x = Tensors.fromString("{1,2,2,3}");
+   try {
+   LSMT1.digest(x);
+   fail();
+   } catch (Exception exception) {
+   // ---
+   }
+   }
   public void testGetMinElements() {
     Tensor slackVector = Tensors.fromString("{1,1,1}");
     LexicographicSemiorderMinTracker LSMT1 = LexicographicSemiorderMinTracker.withList(slackVector);
@@ -38,16 +37,26 @@ public class LexicographicSemiorderMinTrackerTest extends TestCase {
     Tensor y = Tensors.fromString("{2,3,1}");
     Tensor z = Tensors.fromString("{3,2,2}");
     Tensor w = Tensors.fromString("{4,1,3}");
+    Tensor u = Tensors.fromString("{1,1,1}");
     LSMT1.digest(x);
     assertTrue(LSMT1.getMinElements().contains(x));
+    LSMT1.digest(y);
+    assertTrue(LSMT1.getMinElements().contains(y));
     LSMT1.digest(z);
     assertFalse(LSMT1.getMinElements().contains(z));
-    LSMT1.digest(y);
-    LSMT1.digest(w);
-    assertTrue(LSMT1.getMinElements().contains(y));
-    Tensor u = Tensors.fromString("{1,1,1}");
+    // LSMT1.digest(w);
     LSMT1.digest(u);
     assertTrue(LSMT1.getMinElements().contains(u));
     assertFalse(LSMT1.getMinElements().contains(y));
   }
+  // public void testPermutations() {
+  // Tensor slackVector = Tensors.fromString("{1,1,1}");
+  // LexicographicSemiorderMinTracker LSMT1 = LexicographicSemiorderMinTracker.withList(slackVector);
+  // Tensor tensor = Permutations.of(Range.of(0, 3));
+  // for (Tensor v : tensor) {
+  // System.out.println(v);
+  // LSMT1.digest(v);
+  // assertTrue(LSMT1.getMinElements().contains(v));
+  // }
+  // }
 }
