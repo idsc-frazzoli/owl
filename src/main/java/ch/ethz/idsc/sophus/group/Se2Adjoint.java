820f6c9b4b872a81e923f217fcda47cf22bb836a
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-04-09 13:01:01 +0200

    se2 adjoint forward and inverse, tests

--- src/main/java/ch/ethz/idsc/sophus/group/Se2Adjoint.java
+++ src/main/java/ch/ethz/idsc/sophus/group/Se2Adjoint.java
@@ -5,6 +5,7 @@ import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.TensorRuntimeException;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Array;
 import ch.ethz.idsc.tensor.opt.TensorUnaryOperator;
 import ch.ethz.idsc.tensor.sca.Cos;
 import ch.ethz.idsc.tensor.sca.Sin;
@@ -16,20 +17,36 @@ import ch.ethz.idsc.tensor.sca.Sin;
  * "Lie Groups for 2D and 3D Transformations", p. 16 */
 // TODO Se2CoveringAdjoint ?
 public class Se2Adjoint implements TensorUnaryOperator {
+  private static final Tensor ORIGIN = Array.zeros(3);
+
+  /** @param xya element from Lie Group SE2 as coordinates {x, y, alpha}
+   * @return */
+  public static TensorUnaryOperator forward(Tensor xya) {
+    return new Se2Adjoint(xya);
+  }
+
+  /** @param xya element from Lie Group SE2 as coordinates {x, y, alpha}
+   * @return */
+  public static TensorUnaryOperator inverse(Tensor xya) {
+    // TODO implementation not ideal
+    return new Se2Adjoint(new Se2GroupElement(xya).inverse().combine(ORIGIN));
+  }
+
+  // ---
   private final Scalar px;
   private final Scalar py;
-  private final Scalar ct;
-  private final Scalar st;
+  private final Scalar ca;
+  private final Scalar sa;
 
   /** @param element from Lie Group SE2 as coordinates {x, y, omega} */
-  public Se2Adjoint(Tensor xya) {
+  private Se2Adjoint(Tensor xya) {
     if (xya.length() != 3)
       throw TensorRuntimeException.of(xya);
     px = xya.Get(0);
     py = xya.Get(1);
-    Scalar th = xya.Get(2);
-    ct = Cos.FUNCTION.apply(th);
-    st = Sin.FUNCTION.apply(th);
+    Scalar al = xya.Get(2);
+    ca = Cos.FUNCTION.apply(al);
+    sa = Sin.FUNCTION.apply(al);
   }
 
   @Override
@@ -38,8 +55,8 @@ public class Se2Adjoint implements TensorUnaryOperator {
     Scalar v = uvw.Get(1);
     Scalar w = uvw.Get(2);
     return Tensors.of( //
-        ct.multiply(u).subtract(st.multiply(v)).add(py.multiply(w)), //
-        st.multiply(u).add(ct.multiply(v)).subtract(px.multiply(w)), //
+        ca.multiply(u).subtract(sa.multiply(v)).add(py.multiply(w)), //
+        sa.multiply(u).add(ca.multiply(v)).subtract(px.multiply(w)), //
         w);
   }
 }
--- src/test/java/ch/ethz/idsc/sophus/group/Se2AdjointTest.java
+++ src/test/java/ch/ethz/idsc/sophus/group/Se2AdjointTest.java
@@ -1,11 +1,16 @@
 // code by jph
 package ch.ethz.idsc.sophus.group;
 
+import java.util.Arrays;
+
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Array;
+import ch.ethz.idsc.tensor.alg.Dimensions;
 import ch.ethz.idsc.tensor.alg.UnitVector;
 import ch.ethz.idsc.tensor.mat.HilbertMatrix;
+import ch.ethz.idsc.tensor.opt.TensorUnaryOperator;
 import ch.ethz.idsc.tensor.pdf.Distribution;
 import ch.ethz.idsc.tensor.pdf.NormalDistribution;
 import ch.ethz.idsc.tensor.pdf.RandomVariate;
@@ -13,20 +18,32 @@ import ch.ethz.idsc.tensor.sca.Chop;
 import junit.framework.TestCase;
 
 public class Se2AdjointTest extends TestCase {
-  public void testRotationFixpointSide() {
-    Se2Adjoint se2Adjoint = new Se2Adjoint(Tensors.vector(0, 1, 0));
-    Tensor tensor = se2Adjoint.apply(Tensors.vector(1, 0, -1));
+  public void testRotationFixpointSideLeft() {
+    TensorUnaryOperator se2Adjoint = Se2Adjoint.inverse(Tensors.vector(0, 1, 0)); // "left rear wheel"
+    Tensor tensor = se2Adjoint.apply(Tensors.vector(1, 0, 1)); // more forward and turn left
+    Chop._13.requireClose(tensor, UnitVector.of(3, 2)); // only rotation
+  }
+
+  public void testRotationSideLeft() {
+    TensorUnaryOperator se2Adjoint = Se2Adjoint.forward(Tensors.vector(0, 1, 0)); // "left rear wheel"
+    Tensor tensor = se2Adjoint.apply(Tensors.vector(1, 0, -1)); // more forward and turn right
     Chop._13.requireClose(tensor, UnitVector.of(3, 2).negate()); // only rotation
   }
 
+  public void testRotationFixpointSideRight() {
+    TensorUnaryOperator se2Adjoint = Se2Adjoint.forward(Tensors.vector(0, -1, 0)); // "right rear wheel"
+    Tensor tensor = se2Adjoint.apply(Tensors.vector(1, 0, -1)); // more forward and turn right
+    Chop._13.requireClose(tensor, Tensors.vector(2, 0, -1)); // rotate and translate
+  }
+
   public void testRotationId() {
-    Se2Adjoint se2Adjoint = new Se2Adjoint(Tensors.vector(0, 0, 2));
+    TensorUnaryOperator se2Adjoint = Se2Adjoint.forward(Tensors.vector(0, 0, 2));
     Tensor tensor = se2Adjoint.apply(Tensors.vector(0, 0, 1));
     Chop._13.requireClose(tensor, UnitVector.of(3, 2)); // same rotation
   }
 
   public void testRotationTranslation() {
-    Se2Adjoint se2Adjoint = new Se2Adjoint(Tensors.vector(1, 0, Math.PI / 2));
+    TensorUnaryOperator se2Adjoint = Se2Adjoint.forward(Tensors.vector(1, 0, Math.PI / 2));
     Tensor tensor = se2Adjoint.apply(Tensors.vector(0, 0, 1));
     Chop._13.requireClose(tensor, Tensors.vector(0, -1, 1));
   }
@@ -34,7 +51,7 @@ public class Se2AdjointTest extends TestCase {
   public void testTranslate() {
     Distribution distribution = NormalDistribution.standard();
     for (int count = 0; count < 100; ++count) {
-      Se2Adjoint se2Adjoint = new Se2Adjoint(RandomVariate.of(distribution, 2).append(RealScalar.ZERO));
+      TensorUnaryOperator se2Adjoint = Se2Adjoint.forward(RandomVariate.of(distribution, 2).append(RealScalar.ZERO));
       Tensor uvw = RandomVariate.of(distribution, 2).append(RealScalar.ZERO);
       Tensor tensor = se2Adjoint.apply(uvw);
       Chop._13.requireClose(tensor, uvw); // only translation
@@ -43,24 +60,84 @@ public class Se2AdjointTest extends TestCase {
 
   public void testComparison() {
     Distribution distribution = NormalDistribution.standard();
-    for (int count = 0; count < 100; ++count) {
+    for (int count = 0; count < 10; ++count) {
       Tensor xya = RandomVariate.of(distribution, 3);
-      Se2Adjoint se2Adjoint = new Se2Adjoint(xya);
+      TensorUnaryOperator se2Adjoint = Se2Adjoint.forward(xya);
       Se2AdjointComp se2AdjointComp = new Se2AdjointComp(xya);
       Tensor uvw = RandomVariate.of(distribution, 3);
       Chop._13.requireClose(se2Adjoint.apply(uvw), se2AdjointComp.apply(uvw)); // only translation
     }
   }
 
+  public void testFwdInv() {
+    Distribution distribution = NormalDistribution.standard();
+    for (int count = 0; count < 10; ++count) {
+      Tensor xya = RandomVariate.of(distribution, 3);
+      TensorUnaryOperator forward = Se2Adjoint.forward(xya);
+      TensorUnaryOperator inverse = Se2Adjoint.inverse(xya);
+      Tensor uvw = RandomVariate.of(distribution, 3);
+      Chop._13.requireClose(inverse.apply(forward.apply(uvw)), uvw);
+      Chop._13.requireClose(forward.apply(inverse.apply(uvw)), uvw);
+    }
+  }
+
+  public void testNonCovering() {
+    Distribution distribution = NormalDistribution.standard();
+    for (int count = 0; count < 10; ++count) {
+      Tensor xya = RandomVariate.of(distribution, 3);
+      Tensor uvw = RandomVariate.of(distribution, 3);
+      Tensor res = Se2Adjoint.forward(xya).apply(uvw);
+      for (int v = -3; v <= 3; ++v) {
+        Tensor xyp = xya.copy();
+        xyp.set(RealScalar.of(v * 2 * Math.PI)::add, 2);
+        Chop._13.requireClose(res, Se2Adjoint.forward(xyp).apply(uvw));
+      }
+    }
+  }
+
+  public void testSimple() {
+    Distribution distribution = NormalDistribution.standard();
+    for (int count = 0; count < 10; ++count) {
+      Tensor g = RandomVariate.of(distribution, 3);
+      TensorUnaryOperator se2Adjoint = Se2Adjoint.forward(g);
+      Tensor u_w = RandomVariate.of(distribution, 3);
+      Tensor out = se2Adjoint.apply(u_w);
+      assertEquals(Dimensions.of(out), Arrays.asList(3));
+      Tensor g_i = new Se2GroupElement(g).inverse().combine(Array.zeros(3));
+      TensorUnaryOperator se2Inverse = Se2Adjoint.forward(g_i);
+      Tensor apply = se2Inverse.apply(out);
+      Chop._13.requireClose(u_w, apply);
+    }
+  }
+
+  public void testUnits() {
+    TensorUnaryOperator se2Adjoint = Se2Adjoint.forward(Tensors.fromString("{2[m],3[m],4}"));
+    Tensor tensor = se2Adjoint.apply(Tensors.fromString("{7[m*s^-1],-5[m*s^-1],1[s^-1]}"));
+    Chop._13.requireClose(tensor, //
+        Tensors.fromString("{-5.359517822584925[m*s^-1], -4.029399362837438[m*s^-1], 1[s^-1]}"));
+  }
+
   public void testFail() {
     try {
-      new Se2Adjoint(RealScalar.ONE);
+      Se2Adjoint.forward(RealScalar.ONE);
       fail();
     } catch (Exception exception) {
       // ---
     }
     try {
-      new Se2Adjoint(HilbertMatrix.of(3));
+      Se2Adjoint.forward(HilbertMatrix.of(3));
+      fail();
+    } catch (Exception exception) {
+      // ---
+    }
+    try {
+      Se2Adjoint.inverse(RealScalar.ONE);
+      fail();
+    } catch (Exception exception) {
+      // ---
+    }
+    try {
+      Se2Adjoint.inverse(HilbertMatrix.of(3));
       fail();
     } catch (Exception exception) {
       // ---
