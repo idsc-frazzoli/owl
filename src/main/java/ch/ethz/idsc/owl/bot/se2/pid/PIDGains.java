792840375f16879b49ee166755cbbe7bb6fbe009
maximilien <maximilien.picquet@gmail.com>
2019-04-13 13:45:56 +0200

    adapted for retina

--- src/main/java/ch/ethz/idsc/owl/bot/se2/pid/PIDGains.java
+++ src/main/java/ch/ethz/idsc/owl/bot/se2/pid/PIDGains.java
@@ -5,10 +5,12 @@ import ch.ethz.idsc.tensor.Scalar;
 
 public class PIDGains {
   public final Scalar Kp;
+  public final Scalar Ki;
   public final Scalar Kd;
 
-  public PIDGains(Scalar Kp, Scalar Kd) {
+  public PIDGains(Scalar Kp, Scalar Ki, Scalar Kd) {
     this.Kp = Kp;
+    this.Ki = Ki;
     this.Kd = Kd;
     // TODO JPH how to GlobalAssert.that(Kd and Kd has correct unit); 
   }
