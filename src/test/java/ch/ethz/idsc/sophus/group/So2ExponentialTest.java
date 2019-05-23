3e91f5e44678c7a1cd2745c6554cac623f8657ba
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-05-23 22:17:10 +0200

    so2 exponential test

--- src/test/java/ch/ethz/idsc/sophus/group/So2ExponentialTest.java
+++ src/test/java/ch/ethz/idsc/sophus/group/So2ExponentialTest.java
@@ -5,8 +5,10 @@ import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.alg.Transpose;
 import ch.ethz.idsc.tensor.mat.IdentityMatrix;
+import ch.ethz.idsc.tensor.pdf.Distribution;
+import ch.ethz.idsc.tensor.pdf.NormalDistribution;
+import ch.ethz.idsc.tensor.pdf.RandomVariate;
 import ch.ethz.idsc.tensor.sca.Chop;
-import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class So2ExponentialTest extends TestCase {
@@ -26,9 +28,10 @@ public class So2ExponentialTest extends TestCase {
   }
 
   public void testTranspose() {
-    Tensor vector = Tensors.vector(Math.random());
+    Distribution distribution = NormalDistribution.standard();
+    Tensor vector = RandomVariate.of(distribution, 1);
     Tensor m1 = So2Exponential.INSTANCE.exp(vector);
     Tensor m2 = Transpose.of(So2Exponential.INSTANCE.exp(vector));
-    Assert.assertEquals(IdentityMatrix.of(2), m1.dot(m2));
+    Chop._12.requireClose(IdentityMatrix.of(2), m1.dot(m2));
   }
 }
