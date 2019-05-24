8a62152fb456499fb11418746e2b29680ac73b5f
rot8 <oliverbr@ethz.ch>
2019-05-24 10:21:12 +0200

    generalization from He(3) to He(n)

--- src/main/java/ch/ethz/idsc/sophus/group/HeBiinvariantMean.java
+++ src/main/java/ch/ethz/idsc/sophus/group/HeBiinvariantMean.java
@@ -6,11 +6,10 @@ import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
-import ch.ethz.idsc.tensor.alg.VectorQ;
 
-/** @param sequence of (x,y,z) points in He(3) and weights non-negative and normalized
- * @return associated biinvariant meanb which is the solution to the barycentric equation
- * Execption is thrown if
+/** @param sequence of (x,y,z) points in He(n) of shape ((x1, ... , xm),(y1, ... , ym),z)
+ * @param normalized non negative weights
+ * @return associated biinvariant mean which is the solution to the barycentric equation
  * Source: "Bi-invariant Means in Lie Groups. Application toLeft-invariant Polyaffine Transformations." p32
  * Vincent Arsigny — Xavier Pennec — Nicholas Ayache */
 public enum HeBiinvariantMean implements BiinvariantMeanInterface {
@@ -20,12 +19,11 @@ public enum HeBiinvariantMean implements BiinvariantMeanInterface {
 
   @Override
   public Tensor mean(Tensor sequence, Tensor weights) {
-    VectorQ.requireLength(sequence.get(0), 3);
-    Scalar xMean = (Scalar) weights.dot(Tensor.of(sequence.stream().map(xyz -> xyz.Get(0))));
-    Scalar yMean = (Scalar) weights.dot(Tensor.of(sequence.stream().map(xyz -> xyz.Get(1))));
+    Tensor xMean = weights.dot(Tensor.of(sequence.stream().map(xyz -> xyz.get(0))));
+    Tensor yMean = weights.dot(Tensor.of(sequence.stream().map(xyz -> xyz.get(1))));
     Scalar zMean = (Scalar) weights.dot(Tensor.of(sequence.stream().map(xyz -> xyz.Get(2))));
-    Scalar xyMean = (Scalar) weights.dot(Tensor.of(sequence.stream().map(xyz -> xyz.Get(0).multiply(xyz.Get(1)))));
-    zMean = zMean.add(xMean.multiply(yMean).subtract(xyMean).divide(TWO));
+    Scalar xyMean = (Scalar) weights.dot(Tensor.of(sequence.stream().map(xyz -> xyz.get(0).dot(xyz.get(1)))));
+    zMean = zMean.add(xMean.dot(yMean).subtract(xyMean).divide(TWO));
     return Tensors.of(xMean, yMean, zMean);
   }
 }
