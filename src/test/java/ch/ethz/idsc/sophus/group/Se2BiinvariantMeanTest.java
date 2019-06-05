6d1cd024ebcd9917eef41347fd68a68a0b46ff8f
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-06-05 23:34:57 +0200

    se2 biinvariant mean, affine q test

--- src/test/java/ch/ethz/idsc/sophus/group/Se2BiinvariantMeanTest.java
+++ src/test/java/ch/ethz/idsc/sophus/group/Se2BiinvariantMeanTest.java
@@ -1,11 +1,18 @@
 // code by ob
 package ch.ethz.idsc.sophus.group;
 
+import java.util.stream.IntStream;
+
 import ch.ethz.idsc.tensor.RealScalar;
 import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.alg.Range;
+import ch.ethz.idsc.tensor.alg.Transpose;
+import ch.ethz.idsc.tensor.io.Primitives;
+import ch.ethz.idsc.tensor.lie.Permutations;
 import ch.ethz.idsc.tensor.opt.Pi;
+import ch.ethz.idsc.tensor.red.Total;
 import ch.ethz.idsc.tensor.sca.Chop;
 import ch.ethz.idsc.tensor.sca.Power;
 import junit.framework.TestCase;
@@ -75,7 +82,6 @@ public class Se2BiinvariantMeanTest extends TestCase {
     Chop._14.requireClose(Tensors.vector(0, 0, 0.6), actual);
   }
 
-  //
   public void testOrderInvariance() {
     Tensor p = Tensors.vector(4.9, 4.9, 0.9);
     Tensor q = Tensors.vector(5.0, 5.0, 1.0);
@@ -91,6 +97,60 @@ public class Se2BiinvariantMeanTest extends TestCase {
     Chop._14.requireClose(actual1, actual3);
   }
 
+  private static Tensor order(Tensor tensor, int[] index) {
+    return Tensor.of(IntStream.of(index).mapToObj(tensor::get));
+  }
+
+  public void testOrderInvariance2() {
+    Tensor p = Tensors.vector(4.9, 4.9, 0.9);
+    Tensor q = Tensors.vector(5.0, 5.0, 1.0);
+    Tensor r = Tensors.vector(5.1, 5.1, 1.1);
+    Tensor s = Tensors.vector(4.8, 5.2, 1.3);
+    Tensor sequence = Tensors.of(p, q, r, s);
+    Tensor weights = Tensors.vector(3, 2, 1, 4).divide(RealScalar.of(10));
+    Tensor solution = Se2BiinvariantMean.INSTANCE.mean(sequence, weights);
+    Chop._12.requireClose(solution, Tensors.vector(4.911144632104387, 5.064995814659804, 1.1));
+    for (Tensor perm : Permutations.of(Range.of(0, weights.length()))) {
+      int[] index = Primitives.toIntArray(perm);
+      Tensor result = Se2BiinvariantMean.INSTANCE.mean(order(sequence, index), order(weights, index));
+      Chop._12.requireClose(result, solution);
+    }
+  }
+
+  public void testOrderInvariance3() {
+    Tensor p = Tensors.fromString("{4.9[m], 4.9[m], 0.9}");
+    Tensor q = Tensors.fromString("{5.0[m], 5.0[m], 1.0}");
+    Tensor r = Tensors.fromString("{5.1[m], 5.1[m], 1.1}");
+    Tensor s = Tensors.fromString("{4.8[m], 5.2[m], 1.3}");
+    Tensor sequence = Tensors.of(p, q, r, s);
+    Tensor weights = Tensors.vector(3, 2, 1, 4).divide(RealScalar.of(10));
+    Tensor solution = Se2BiinvariantMean.INSTANCE.mean(sequence, weights);
+    Chop._12.requireClose(solution, Tensors.fromString("{4.911144632104387[m], 5.064995814659804[m], 1.1}"));
+    for (Tensor perm : Permutations.of(Range.of(0, weights.length()))) {
+      int[] index = Primitives.toIntArray(perm);
+      Tensor result = Se2BiinvariantMean.INSTANCE.mean(order(sequence, index), order(weights, index));
+      Chop._12.requireClose(result, solution);
+    }
+  }
+
+  public void testOrderInvarianceNegative() {
+    Tensor p = Tensors.vector(14.9, -4.9, 10.9);
+    Tensor q = Tensors.vector(15.0, -5.0, 11.0);
+    Tensor r = Tensors.vector(15.1, -5.1, 11.1);
+    Tensor s = Tensors.vector(14.8, -5.2, 11.3);
+    Tensor sequence = Tensors.of(p, q, r, s);
+    Tensor mask = Tensors.vector(3, 2, -1, 4);
+    Tensor weights = mask.divide(Total.ofVector(mask));
+    Tensor solution = Se2BiinvariantMean.INSTANCE.mean(sequence, weights);
+    System.out.println(solution);
+    Chop._12.requireClose(solution, Tensors.vector(14.83619642851975, -5.043678108261259, -1.466370614359171));
+    for (Tensor perm : Permutations.of(Range.of(0, weights.length()))) {
+      int[] index = Primitives.toIntArray(perm);
+      Tensor result = Se2BiinvariantMean.INSTANCE.mean(order(sequence, index), order(weights, index));
+      Chop._12.requireClose(result, solution);
+    }
+  }
+
   public void testCombined() {
     Tensor sequence = Tensors.empty();
     Tensor p = Tensors.vector(1, 1, 0.1);
@@ -102,20 +162,18 @@ public class Se2BiinvariantMeanTest extends TestCase {
     Chop._14.requireClose(expected, actual);
   }
 
+  public void testFunctionM() {
+    Tensor m1 = Se2BiinvariantMean.M(RealScalar.of(-.1));
+    Tensor m2 = Se2BiinvariantMean.M(RealScalar.of(+.1));
+    Chop._10.requireClose(m1, Transpose.of(m2));
+  }
+
   public void testFail() {
     Tensor p = Tensors.vector(0, 0, 0);
     Tensor sequence = Tensors.of(p, p, p);
     try {
       // non-normalized weights fail
       Tensor weights = Tensors.vector(1, 1, 1);
-      Se2BiinvariantMean.INSTANCE.mean(sequence, weights);
-      fail();
-    } catch (Exception exception) {
-      // ---
-    }
-    try {
-      // non-positive weights fail
-      Tensor weights = Tensors.vector(-0.2, 1.4, -0.2);
       Se2BiinvariantMean.INSTANCE.mean(sequence, weights);
       fail();
     } catch (Exception exception) {
