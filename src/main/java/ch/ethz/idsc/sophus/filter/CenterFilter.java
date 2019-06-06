ac75f936fb94425f5459207a75c58b32da43e914
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-06-06 10:32:51 +0200

    geodesic adaptive center filter obsolete

--- src/main/java/ch/ethz/idsc/sophus/filter/CenterFilter.java
+++ src/main/java/ch/ethz/idsc/sophus/filter/CenterFilter.java
@@ -4,38 +4,38 @@ package ch.ethz.idsc.sophus.filter;
 import java.util.Objects;
 
 import ch.ethz.idsc.tensor.Tensor;
-import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.Unprotect;
 import ch.ethz.idsc.tensor.opt.TensorUnaryOperator;
 
-/** Hint: the following center operators are typically used
- * {@link GeodesicCenter}
- * {@link BiinvariantMeanCenter} */
 public class CenterFilter implements TensorUnaryOperator {
-  /** @param geodesicCenter
+  /** Hint: the following tensorUnaryOperator are typically used
+   * {@link GeodesicCenter}, and {@link BiinvariantMeanCenter}
+   * 
+   * @param tensorUnaryOperator
    * @param radius
    * @return
-   * @throws Exception if given geodesicCenter is null */
-  public static TensorUnaryOperator of(TensorUnaryOperator geodesicCenter, int radius) {
-    return new CenterFilter(Objects.requireNonNull(geodesicCenter), radius);
+   * @throws Exception if given tensorUnaryOperator is null */
+  public static TensorUnaryOperator of(TensorUnaryOperator tensorUnaryOperator, int radius) {
+    return new CenterFilter(Objects.requireNonNull(tensorUnaryOperator), radius);
   }
 
   // ---
-  private final TensorUnaryOperator geodesicCenter;
+  private final TensorUnaryOperator tensorUnaryOperator;
   private final int radius;
 
-  private CenterFilter(TensorUnaryOperator geodesicCenter, int radius) {
-    this.geodesicCenter = geodesicCenter;
+  private CenterFilter(TensorUnaryOperator tensorUnaryOperator, int radius) {
+    this.tensorUnaryOperator = tensorUnaryOperator;
     this.radius = radius;
   }
 
   @Override
   public Tensor apply(Tensor tensor) {
-    Tensor result = Tensors.empty();
+    Tensor result = Unprotect.empty(tensor.length());
     for (int index = 0; index < tensor.length(); ++index) {
       int lo = Math.max(0, index - radius);
       int hi = Math.min(index + radius, tensor.length() - 1);
       int delta = Math.min(index - lo, hi - index);
-      result.append(geodesicCenter.apply(tensor.extract(index - delta, index + delta + 1)));
+      result.append(tensorUnaryOperator.apply(tensor.extract(index - delta, index + delta + 1)));
     }
     return result;
   }
