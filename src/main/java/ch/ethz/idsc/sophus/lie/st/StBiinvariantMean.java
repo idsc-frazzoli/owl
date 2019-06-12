8d615e44f03eef21ce8ba4aac832f9875be532e6
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-06-12 20:40:45 +0200

    sc biinvariant mean

--- src/main/java/ch/ethz/idsc/sophus/lie/st/StBiinvariantMean.java
+++ src/main/java/ch/ethz/idsc/sophus/lie/st/StBiinvariantMean.java
@@ -2,16 +2,12 @@
 package ch.ethz.idsc.sophus.lie.st;
 
 import ch.ethz.idsc.sophus.lie.BiinvariantMean;
-import ch.ethz.idsc.sophus.math.win.AffineQ;
-import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.sophus.lie.sc.ScBiinvariantMean;
+import ch.ethz.idsc.sophus.lie.sc.ScSkew;
 import ch.ethz.idsc.tensor.Scalar;
-import ch.ethz.idsc.tensor.Scalars;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.red.Total;
-import ch.ethz.idsc.tensor.sca.Exp;
-import ch.ethz.idsc.tensor.sca.Log;
-import ch.ethz.idsc.tensor.sca.ScalarUnaryOperator;
 
 /** @param sequence of (lambda_i, t_i) points in ST(n) and weights non-negative and normalized
  * @return associated biinvariant mean which is the solution to the barycentric equation
@@ -27,34 +23,15 @@ import ch.ethz.idsc.tensor.sca.ScalarUnaryOperator;
  * by Vincent Arsigny, Xavier Pennec, Nicholas Ayache, p.29 */
 public enum StBiinvariantMean implements BiinvariantMean {
   INSTANCE;
-  // ---
-  private static class Alpha implements ScalarUnaryOperator {
-    private final Scalar lambdaBar;
-
-    public Alpha(Scalar lambdaBar) {
-      this.lambdaBar = lambdaBar;
-    }
-
-    @Override
-    public Scalar apply(Scalar lambda) {
-      Scalar ratio = lambda.divide(lambdaBar);
-      Scalar den = ratio.subtract(RealScalar.ONE);
-      return Scalars.isZero(den) //
-          ? RealScalar.ONE // Limit[Log[p]/(p - 1), p -> 1] == 1
-          : Log.FUNCTION.apply(ratio).divide(den);
-    }
-  }
-
   @Override // from BiinvariantMean
   public Tensor mean(Tensor sequence, Tensor weights) {
-    AffineQ.require(weights);
     Tensor lambdas = sequence.get(Tensor.ALL, 0);
-    // Reference 1, p.27
-    Scalar lambdaBar = Exp.FUNCTION.apply(lambdas.map(Log.FUNCTION).dot(weights).Get());
-    Tensor prod = weights.pmul(lambdas.map(new Alpha(lambdaBar)));
+    // Reference 1, p.27 "weighted geometric mean of scalings"
+    Scalar scmean = ScBiinvariantMean.INSTANCE.mean(lambdas, weights);
+    Tensor prod = weights.pmul(lambdas.map(new ScSkew(scmean)));
     Tensor sum = prod.dot(sequence.get(Tensor.ALL, 1));
     return Tensors.of( //
-        lambdaBar, // "weighted geometric mean of scalings"
+        scmean, //
         sum.divide(Total.ofVector(prod))); // "scalings reweighed arithmetic mean of translations"
   }
 }
