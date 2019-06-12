6debb3415cfc2516c826222cc37b2d1ccddbb61b
JoelGaechter <joelgaechter@bluewin.ch>
2019-06-12 14:22:14 +0200

    clothoid transition

---
+++ src/main/java/ch/ethz/idsc/owl/bot/se2/rrts/ClothoidTransition.java
@@ -0,0 +1,36 @@
+// code by gjoel
+package ch.ethz.idsc.owl.bot.se2.rrts;
+
+import ch.ethz.idsc.sophus.curve.ClothoidCurve;
+import ch.ethz.idsc.sophus.planar.ClothoidDistance;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Scalars;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.TensorRuntimeException;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.opt.ScalarTensorFunction;
+
+/* package */ class ClothoidTransition extends Se2Transition {
+  public ClothoidTransition(Tensor start, Tensor end, Scalar radius) {
+    super(start, end);
+  }
+
+  @Override // from Transition
+  public Scalar length() {
+    return ClothoidDistance.INSTANCE.distance(start(), end());
+  }
+
+  @Override // from Transition
+  public Tensor sampled(Scalar ofs, Scalar dt) {
+    if (Scalars.lessThan(dt, ofs))
+      throw TensorRuntimeException.of(ofs, dt);
+    Scalar length = length();
+    ScalarTensorFunction scalarTensorFunction = scalar -> ClothoidCurve.INSTANCE.split(start(), end(), scalar);
+    Tensor tensor = Tensors.empty();
+    while (Scalars.lessThan(ofs, length)) {
+      tensor.append(scalarTensorFunction.apply(ofs.divide(length)));
+      ofs = ofs.add(dt);
+    }
+    return tensor;
+  }
+}
