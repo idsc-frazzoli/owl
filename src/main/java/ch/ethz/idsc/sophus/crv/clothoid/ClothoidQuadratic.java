2e6dc7c8d4eed304a0b00c001131e38b2e6cdfd1
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-06-13 07:52:44 +0200

    intersection 2d

--- src/main/java/ch/ethz/idsc/sophus/crv/clothoid/ClothoidQuadratic.java
+++ src/main/java/ch/ethz/idsc/sophus/crv/clothoid/ClothoidQuadratic.java
@@ -10,14 +10,14 @@ import ch.ethz.idsc.tensor.sca.ScalarUnaryOperator;
 
 /* package */ class ClothoidQuadratic implements ScalarUnaryOperator {
   private static final Scalar _3 = RealScalar.of(+3);
-  private static final Scalar N4 = RealScalar.of(-4);
   // ---
   private final ScalarUnaryOperator series;
 
   public ClothoidQuadratic(Scalar b0, Scalar bm, Scalar b1) {
-    Scalar c1 = b0.multiply(_3).add(bm.multiply(N4)).add(b1);
-    Scalar c2 = b0.add(b1).subtract(bm).subtract(bm);
-    series = Series.of(Tensors.of(b0, c1.negate(), c2.add(c2)));
+    Scalar b2 = bm.add(bm);
+    Scalar c1 = b2.add(b2).subtract(b0.multiply(_3).add(b1));
+    Scalar c2 = b0.add(b1).subtract(b2);
+    series = Series.of(Tensors.of(b0, c1, c2.add(c2)));
   }
 
   @Override
@@ -25,7 +25,7 @@ import ch.ethz.idsc.tensor.sca.ScalarUnaryOperator;
     return ComplexScalar.unit(angle(s));
   }
 
-  /* package */ Scalar angle(Scalar s) {
+  public Scalar angle(Scalar s) {
     return series.apply(s);
   }
 }
