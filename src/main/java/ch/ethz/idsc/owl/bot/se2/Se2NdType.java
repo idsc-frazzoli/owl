6e23830b0a63e41bcfdd7b4b8f187000508d676a
JoelGaechter <joelgaechter@bluewin.ch>
2019-06-14 12:46:01 +0200

    se2 rrts node collection edits

---
+++ src/main/java/ch/ethz/idsc/owl/bot/se2/Se2NdType.java
@@ -0,0 +1,36 @@
+// code by gjoel
+package ch.ethz.idsc.owl.bot.se2;
+
+import ch.ethz.idsc.owl.data.nd.NdCenterInterface;
+import ch.ethz.idsc.sophus.math.Extract2D;
+import ch.ethz.idsc.tensor.Tensor;
+
+/* package */ enum Se2NdType {
+  EUCLIDEAN {
+    @Override
+    public Tensor convert(Tensor tensor) {
+      return Extract2D.FUNCTION.apply(tensor);
+    }
+
+    @Override
+    public NdCenterInterface getNdCenterInterface(Tensor tensor) {
+      return NdCenterInterface.euclidean(convert(tensor));
+    }
+  },
+  CLOTHOID {
+    @Override
+    public NdCenterInterface getNdCenterInterface(Tensor tensor) {
+      return NdCenterInterface.clothoid(convert(tensor));
+    }
+  };
+
+  /** @param tensor
+   * @return tensor in right format */
+  public Tensor convert(Tensor tensor) {
+    return tensor;
+  }
+
+  /**@param tensor
+   * @return */
+  public abstract NdCenterInterface getNdCenterInterface(Tensor tensor);
+}
