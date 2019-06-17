8fc58f7e5ddc2ca485723bd9778c81d3ea1541b8
JoelGaechter <joelgaechter@bluewin.ch>
2019-06-17 12:36:13 +0200

    se2 image demo

---
+++ src/main/java/ch/ethz/idsc/owl/bot/se2/rrts/Se2ImageDemo.java
@@ -0,0 +1,59 @@
+// code by jph, gjoel
+package ch.ethz.idsc.owl.bot.se2.rrts;
+
+import java.util.Random;
+
+import ch.ethz.idsc.owl.bot.r2.ImageRegions;
+import ch.ethz.idsc.owl.bot.se2.Se2RrtsNodeCollection;
+import ch.ethz.idsc.owl.bot.util.RegionRenders;
+import ch.ethz.idsc.owl.gui.win.OwlyFrame;
+import ch.ethz.idsc.owl.gui.win.OwlyGui;
+import ch.ethz.idsc.owl.math.region.ImageRegion;
+import ch.ethz.idsc.owl.math.sample.BoxRandomSample;
+import ch.ethz.idsc.owl.math.sample.RandomSampleInterface;
+import ch.ethz.idsc.owl.rrts.adapter.LengthCostFunction;
+import ch.ethz.idsc.owl.rrts.adapter.RrtsNodes;
+import ch.ethz.idsc.owl.rrts.adapter.SampledTransitionRegionQuery;
+import ch.ethz.idsc.owl.rrts.core.DefaultRrts;
+import ch.ethz.idsc.owl.rrts.core.Rrts;
+import ch.ethz.idsc.owl.rrts.core.RrtsNode;
+import ch.ethz.idsc.owl.rrts.core.RrtsNodeCollection;
+import ch.ethz.idsc.owl.rrts.core.TransitionRegionQuery;
+import ch.ethz.idsc.owl.rrts.core.TransitionSpace;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.Tensors;
+import ch.ethz.idsc.tensor.opt.Pi;
+
+/* package */ enum Se2ImageDemo {
+  ;
+  private static final Random RANDOM = new Random();
+
+  public static void main(String[] args) throws Exception {
+    ImageRegion imageRegion = //
+        ImageRegions.loadFromRepository("/io/track0_100.png", Tensors.vector(7, 7), false);
+    Tensor lbounds = imageRegion.origin().copy().append(RealScalar.ZERO).unmodifiable();
+    Tensor ubounds = imageRegion.range().copy().append(Pi.TWO).unmodifiable();
+    RrtsNodeCollection rrtsNodeCollection = Se2RrtsNodeCollection.clothoid(lbounds, ubounds);
+    TransitionRegionQuery transitionRegionQuery = new SampledTransitionRegionQuery( //
+        imageRegion, RealScalar.of(0.05));
+    // ---
+    TransitionSpace transitionSpace = new Se2TransitionSpace<>(ClothoidTransition.class);
+    Rrts rrts = new DefaultRrts(transitionSpace, rrtsNodeCollection, transitionRegionQuery, LengthCostFunction.IDENTITY);
+    RrtsNode root = rrts.insertAsNode(Tensors.vector(0, 0, 0), 5).get();
+    OwlyFrame owlyFrame = OwlyGui.start();
+    owlyFrame.configCoordinateOffset(60, 477);
+    owlyFrame.jFrame.setBounds(100, 100, 550, 550);
+    owlyFrame.addBackground(RegionRenders.create(imageRegion));
+    RandomSampleInterface randomSampleInterface = BoxRandomSample.of(lbounds, ubounds);
+    int frame = 0;
+    while (frame++ < 20 && owlyFrame.jFrame.isVisible()) {
+      for (int c = 0; c < 50; ++c)
+        rrts.insertAsNode(randomSampleInterface.randomSample(RANDOM), 15);
+      owlyFrame.setRrts(transitionSpace, root, transitionRegionQuery);
+      Thread.sleep(10);
+    }
+    System.out.println(rrts.rewireCount());
+    RrtsNodes.costConsistency(root, transitionSpace, LengthCostFunction.IDENTITY);
+  }
+}
