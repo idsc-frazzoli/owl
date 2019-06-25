f63c76a82eaee1b73f8e263ac9d6412500f2fd61
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-06-25 08:43:09 +0200

    uniform dataset filter demo shows spectrogram

---
+++ src/main/java/ch/ethz/idsc/tensor/Spectrogram.java
@@ -0,0 +1,34 @@
+// code by jph
+package ch.ethz.idsc.tensor;
+
+import java.util.function.Function;
+
+import ch.ethz.idsc.tensor.img.ArrayPlot;
+import ch.ethz.idsc.tensor.img.ColorDataGradients;
+import ch.ethz.idsc.tensor.sca.Abs;
+
+/** inspired by
+ * <a href="https://reference.wolfram.com/language/ref/Spectrogram.html">Spectrogram</a> */
+public enum Spectrogram {
+  ;
+  /** @param tensor
+   * @return */
+  public static Tensor of(Tensor tensor) {
+    return of(tensor, ColorDataGradients.VISIBLESPECTRUM);
+  }
+
+  /** @param tensor
+   * @param function
+   * @return */
+  public static Tensor of(Tensor tensor, Function<Scalar, ? extends Tensor> function) {
+    return ArrayPlot.of(array(tensor), function);
+  }
+
+  /** @param tensor
+   * @return */
+  public static Tensor array(Tensor tensor) {
+    Tensor spectrogram = SpectrogramArray.of(tensor).map(Abs.FUNCTION);
+    int half = Unprotect.dimension1(spectrogram) / 2;
+    return Tensors.vector(i -> spectrogram.get(Tensor.ALL, half - i - 1), half);
+  }
+}
