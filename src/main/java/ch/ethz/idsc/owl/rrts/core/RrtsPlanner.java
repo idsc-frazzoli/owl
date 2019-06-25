eeab42eb9971a8d5e76ae9c6d6560d901a8d7fe0
JoelGaechter <joelgaechter@bluewin.ch>
2019-06-25 15:26:00 +0200

    comments

--- src/main/java/ch/ethz/idsc/owl/rrts/core/RrtsPlanner.java
+++ src/main/java/ch/ethz/idsc/owl/rrts/core/RrtsPlanner.java
@@ -1,53 +1,61 @@
 // code by jph
 package ch.ethz.idsc.owl.rrts.core;
 
+import java.util.Comparator;
 import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Random;
 
 import ch.ethz.idsc.owl.data.tree.NodeCostComparator;
+import ch.ethz.idsc.owl.data.tree.StateCostNode;
 import ch.ethz.idsc.owl.glc.core.ExpandInterface;
 import ch.ethz.idsc.owl.math.sample.RandomSampleInterface;
 
 public class RrtsPlanner implements ExpandInterface<RrtsNode> {
+  private static final int K_NEAREST = 12;
   private static final RrtsNode DUMMY = new RrtsNodeImpl(null, null);
   private static final Random RANDOM = new Random();
   // ---
   private final Rrts rrts;
   private final RandomSampleInterface spaceSample;
   private final RandomSampleInterface goalSample;
-  private final Queue<RrtsNode> queue = new PriorityQueue<>(NodeCostComparator.INSTANCE);
+  private final Queue<RrtsNode> queue;
 
   /** @param rrts with root already inserted
-   * @param obstacleQuery
    * @param spaceSample
    * @param goalSample generates samples in goal region */
   public RrtsPlanner(Rrts rrts, RandomSampleInterface spaceSample, RandomSampleInterface goalSample) {
+    this(rrts, spaceSample, goalSample, NodeCostComparator.INSTANCE); // TODO use other criterion: distance from goal center ...?
+  }
+
+  /** @param rrts with root already inserted
+   * @param spaceSample
+   * @param goalSample generates samples in goal region
+   * @param nodeComparator*/
+  public RrtsPlanner(Rrts rrts, RandomSampleInterface spaceSample, RandomSampleInterface goalSample, Comparator<StateCostNode> nodeComparator) {
     this.rrts = rrts;
     this.spaceSample = spaceSample;
     this.goalSample = goalSample;
+    queue = new PriorityQueue<>(nodeComparator);
   }
 
-  @Override
+  @Override // from ExpandInterface
   public Optional<RrtsNode> pollNext() {
-    return Optional.of(DUMMY);
+    return Optional.of(DUMMY); // never used, see expand(RrtsNode node)
   }
 
-  @Override
-  public void expand(RrtsNode node) {
-    final int k_nearest = 12; // magic const
-    rrts.insertAsNode(spaceSample.randomSample(RANDOM), k_nearest);
+  @Override // from ExpandInterface
+  public void expand(RrtsNode node) { // node is not used, instead new random sample
+    rrts.insertAsNode(spaceSample.randomSample(RANDOM), K_NEAREST);
     if (queue.isEmpty()) { // TODO RRTS logic not final
-      Optional<RrtsNode> optional = rrts.insertAsNode(goalSample.randomSample(RANDOM), k_nearest);
-      if (optional.isPresent())
-        queue.add(optional.get());
+      rrts.insertAsNode(goalSample.randomSample(RANDOM), K_NEAREST).ifPresent(queue::add);
     }
   }
 
-  @Override
+  @Override // from ExpandInterface
   public Optional<RrtsNode> getBest() {
-    return Optional.ofNullable(queue.peek()); // TODO use other criterion: distance from goal center ...?
+    return Optional.ofNullable(queue.peek());
   }
 
   public TransitionRegionQuery getObstacleQuery() {
