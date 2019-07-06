3f5576f1d46bc8161984f38283b4567968a15c96
rot8 <oliverbr@ethz.ch>
2019-07-06 16:28:28 +0200

    50Hz data

--- src/main/java/ch/ethz/idsc/sophus/app/ob/FourierWindowPlot.java
+++ src/main/java/ch/ethz/idsc/sophus/app/ob/FourierWindowPlot.java
@@ -14,7 +14,8 @@ import org.jfree.graphics2d.svg.SVGGraphics2D;
 import org.jfree.graphics2d.svg.SVGUtils;
 
 import ch.ethz.idsc.sophus.app.api.GeodesicDisplay;
-import ch.ethz.idsc.sophus.app.api.GokartPoseDataV1;
+import ch.ethz.idsc.sophus.app.api.GokartPoseData;
+import ch.ethz.idsc.sophus.app.api.GokartPoseDataV2;
 import ch.ethz.idsc.sophus.app.api.LieGroupFilters;
 import ch.ethz.idsc.sophus.app.api.Se2GeodesicDisplay;
 import ch.ethz.idsc.sophus.flt.CenterFilter;
@@ -32,7 +33,6 @@ import ch.ethz.idsc.tensor.Scalar;
 import ch.ethz.idsc.tensor.Tensor;
 import ch.ethz.idsc.tensor.Tensors;
 import ch.ethz.idsc.tensor.io.HomeDirectory;
-import ch.ethz.idsc.tensor.io.ResourceData;
 import ch.ethz.idsc.tensor.mat.Fourier;
 import ch.ethz.idsc.tensor.mat.SpectrogramArray;
 import ch.ethz.idsc.tensor.opt.TensorUnaryOperator;
@@ -40,15 +40,51 @@ import ch.ethz.idsc.tensor.qty.Quantity;
 import ch.ethz.idsc.tensor.red.Mean;
 import ch.ethz.idsc.tensor.red.Total;
 import ch.ethz.idsc.tensor.sca.Abs;
+import ch.ethz.idsc.tensor.sca.Arg;
 import ch.ethz.idsc.tensor.sca.Log;
 
 /* package */ class FourierWindowPlot {
   private static final Scalar WINDOW_DURATION = Quantity.of(1, "s");
   private static final Scalar SAMPLING_FREQUENCY = Quantity.of(50, "s^-1");
   private static final TensorUnaryOperator SPECTROGRAM_ARRAY = SpectrogramArray.of(WINDOW_DURATION, SAMPLING_FREQUENCY, 1);
+  private static final GokartPoseData GOKART_POSE_DATA = GokartPoseDataV2.INSTANCE;
 
-  // TODO OB: make logPlot (standard)
-  private static void plot(Tensor data, int radius, String signal, SmoothingKernel smoothingKernel) throws IOException {
+  private static void phasePlot(Tensor data, int radius, String signal, SmoothingKernel smoothingKernel) throws IOException {
+    Tensor yData = Tensors.empty();
+    for (Tensor meanData : data)
+      yData.append(TransferFunctionResponse.FREQUENCY.apply(meanData));
+    // ---
+    Tensor xAxis = Tensors.empty();
+    for (int index = -yData.get(0).length() / 2; index < yData.get(0).length() / 2; ++index) {
+      xAxis.append(RationalScalar.of(index, yData.get(0).length()).multiply(SAMPLING_FREQUENCY));
+    }
+    VisualSet visualSet = new VisualSet();
+    visualSet.setPlotLabel("Lie Group Filters: radius = " + radius + "  Phase Response - $" + signal + "$");
+    visualSet.setAxesLabelX("Frequency $[Hz]$");
+    visualSet.setAxesLabelY("Magnitude $|H(\\Omega)|$");
+    int index = 0;
+    for (Tensor yAxis : yData) {
+      VisualRow visualRow = visualSet.add( //
+          xAxis, //
+          Tensor.of(yAxis.append(yAxis).flatten(1)).extract(xAxis.length() / 2, xAxis.length() * 3 / 2));
+      visualRow.setLabel(LieGroupFilters.values()[index].toString());
+      ++index;
+    }
+    VisualRow visualRow = visualSet.add(//
+        xAxis, //
+        Arg.of(Tensor.of(linearResponse(smoothingKernel, radius).append(linearResponse(smoothingKernel, radius)).flatten(1)).extract(xAxis.length() / 2,
+            xAxis.length() * 3 / 2)));
+    visualRow.setLabel("Linear Filter Response");
+    JFreeChart jFreeChart = ListPlot.of(visualSet);
+    SVGGraphics2D svg = new SVGGraphics2D(600, 400);
+    Rectangle rectangle = new Rectangle(0, 0, 600, 400);
+    jFreeChart.draw(svg, rectangle);
+    String fileNameSVG = "PhaseResponse(" + radius + ")" + signal + ".svg";
+    File fileSVG = HomeDirectory.Pictures(fileNameSVG);
+    SVGUtils.writeToSVG(fileSVG, svg.getSVGElement());
+  }
+
+  private static void magniutdePlot(Tensor data, int radius, String signal, SmoothingKernel smoothingKernel) throws IOException {
     Tensor yData = Tensors.empty();
     for (Tensor meanData : data)
       yData.append(TransferFunctionResponse.MAGNITUDE.apply(meanData));
@@ -58,10 +94,8 @@ import ch.ethz.idsc.tensor.sca.Log;
       xAxis.append(RationalScalar.of(index, yData.get(0).length()).multiply(SAMPLING_FREQUENCY));
     }
     VisualSet visualSet = new VisualSet();
-    // visualSet.setPlotLabel("Lie Group Filters: radius = "+radius+" Magnitude Response - $" + signal + "$");
-    visualSet.setPlotLabel("Lie Group Filters: radius = " + radius + "  Phase Response - $" + signal + "$");
+    visualSet.setPlotLabel("Lie Group Filters: radius = " + radius + "  Magnitude Response - $" + signal + "$");
     visualSet.setAxesLabelX("Frequency $[Hz]$");
-    // visualSet.setAxesLabelY("Phase $H(\\Omega)$");
     visualSet.setAxesLabelY("Magnitude $|H(\\Omega)|$");
     int index = 0;
     for (Tensor yAxis : yData) {
@@ -71,17 +105,17 @@ import ch.ethz.idsc.tensor.sca.Log;
       visualRow.setLabel(LieGroupFilters.values()[index].toString());
       ++index;
     }
+    Tensor reference = decibelConversion(Tensor.of(Abs.of(linearResponse(smoothingKernel, radius).append(linearResponse(smoothingKernel, radius))).flatten(1))
+        .extract(xAxis.length() / 2, xAxis.length() * 3 / 2));
     VisualRow visualRow = visualSet.add(//
         xAxis, //
-        decibelConversion(Tensor.of(linearResponse(smoothingKernel, radius).append(linearResponse(smoothingKernel, radius)).flatten(1))
-            .extract(xAxis.length() / 2, xAxis.length() * 3 / 2)));
+        reference);
     visualRow.setLabel("Linear Filter Response");
     JFreeChart jFreeChart = ListPlot.of(visualSet);
     SVGGraphics2D svg = new SVGGraphics2D(600, 400);
     Rectangle rectangle = new Rectangle(0, 0, 600, 400);
     jFreeChart.draw(svg, rectangle);
-    String fileNameSVG = "Geodesic Center Filter(" + radius + ") Gain " + signal + ".svg";
-    // String fileNameSVG = "Geodesic Center Filter(" + radius + ") Phase " + signal + ".svg";
+    String fileNameSVG = "MagnitudeResponse(" + radius + ")" + signal + ".svg";
     File fileSVG = HomeDirectory.Pictures(fileNameSVG);
     SVGUtils.writeToSVG(fileSVG, svg.getSVGElement());
   }
@@ -99,7 +133,7 @@ import ch.ethz.idsc.tensor.sca.Log;
     for (int index = 0; index < 15; ++index)
       ref.append(RealScalar.ZERO);
     ref = ref.divide(Total.ofVector(ref)).multiply(RealScalar.of(Math.sqrt(ref.length())));
-    return Abs.of(Fourier.of(ref));
+    return Fourier.of(ref);
   }
 
   private static void process(List<String> listData, Map<LieGroupFilters, TensorUnaryOperator> map, int radius, int limit, SmoothingKernel smoothingKernel)
@@ -109,7 +143,7 @@ import ch.ethz.idsc.tensor.sca.Log;
     Tensor smoothedA = Tensors.empty();
     Iterator<String> iterator = listData.iterator();
     for (int index = 0; index < limit; ++index) {
-      Tensor control = Tensor.of(ResourceData.of("/dubilab/app/pose/" + iterator.next() + ".csv").stream().map(row -> row.extract(1, 4)));
+      Tensor control = GOKART_POSE_DATA.getPose(iterator.next(), 1000);
       Tensor tempX = Tensors.empty();
       Tensor tempY = Tensors.empty();
       Tensor tempA = Tensors.empty();
@@ -126,9 +160,12 @@ import ch.ethz.idsc.tensor.sca.Log;
       smoothedY.append(tempY);
       smoothedA.append(tempA);
     }
-    plot(Mean.of(smoothedX), radius, "x", smoothingKernel);
-    plot(Mean.of(smoothedY), radius, "y", smoothingKernel);
-    plot(Mean.of(smoothedA), radius, "a", smoothingKernel);
+    magniutdePlot(Mean.of(smoothedX), radius, "x", smoothingKernel);
+    magniutdePlot(Mean.of(smoothedY), radius, "y", smoothingKernel);
+    magniutdePlot(Mean.of(smoothedA), radius, "a", smoothingKernel);
+    phasePlot(Mean.of(smoothedX), radius, "x", smoothingKernel);
+    phasePlot(Mean.of(smoothedY), radius, "y", smoothingKernel);
+    phasePlot(Mean.of(smoothedA), radius, "a", smoothingKernel);
   }
 
   public static void main(String[] args) throws IOException {
@@ -138,7 +175,7 @@ import ch.ethz.idsc.tensor.sca.Log;
     map.put(LieGroupFilters.GEODESIC, GeodesicCenter.of(geodesicDisplay.geodesicInterface(), smoothingKernel));
     map.put(LieGroupFilters.GEODESIC_MID, GeodesicCenterMidSeeded.of(geodesicDisplay.geodesicInterface(), smoothingKernel));
     map.put(LieGroupFilters.BIINVARIANT_MEAN, BiinvariantMeanCenter.of(geodesicDisplay.biinvariantMean(), smoothingKernel));
-    List<String> listData = GokartPoseDataV1.INSTANCE.list();
+    List<String> listData = GOKART_POSE_DATA.list();
     int limit = 10;
     int rad = 24;
     process(listData, map, rad, limit, smoothingKernel);
