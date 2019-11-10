96e652d5e20e6ca58b31032e25b244dfcf314b96
Jan Hakenberg <jan.hakenberg@gmail.com>
2019-11-10 10:18:17 +0100

    readme, spd exponential test, hermite array

---
+++ src/main/java/ch/ethz/idsc/sophus/app/hs/Hermite1Array.java
@@ -0,0 +1,45 @@
+// code by jph
+package ch.ethz.idsc.sophus.app.hs;
+
+import java.io.File;
+import java.io.IOException;
+
+import ch.ethz.idsc.sophus.crv.subdiv.Hermite1Subdivisions;
+import ch.ethz.idsc.tensor.Parallelize;
+import ch.ethz.idsc.tensor.RationalScalar;
+import ch.ethz.idsc.tensor.RealScalar;
+import ch.ethz.idsc.tensor.Scalar;
+import ch.ethz.idsc.tensor.Tensor;
+import ch.ethz.idsc.tensor.alg.Subdivide;
+import ch.ethz.idsc.tensor.io.HomeDirectory;
+import ch.ethz.idsc.tensor.qty.Quantity;
+import ch.ethz.idsc.tensor.sca.Log;
+import ch.ethz.idsc.tensor.sca.N;
+
+/* package */ class Hermite1Array extends HermiteArray {
+  public Hermite1Array(String name, Scalar period, int levels) throws IOException {
+    super(name, period, levels);
+  }
+
+  private Scalar h1(Scalar lambda, Scalar mu) {
+    return process(Hermite1Subdivisions.of(LIE_GROUP, LIE_EXPONENTIAL, lambda, mu));
+  }
+
+  @Override // from HermiteArray
+  Tensor compute(int rows, int cols) {
+    Tensor lambda = N.DOUBLE.of(Subdivide.of(RationalScalar.of(-3, 4), RationalScalar.of(-1, 6), rows - 1));
+    Tensor mu = N.DOUBLE.of(Subdivide.of(RationalScalar.of(-2, 1), RationalScalar.of(+5, 2), cols - 1));
+    return Parallelize.matrix((i, j) -> h1(lambda.Get(i), mu.Get(j)), rows, cols);
+  }
+
+  public static void main(String[] args) throws IOException {
+    int levels = 4;
+    HermiteArray hermiteArray = //
+        new Hermite1Array("20190701T163225_01", Quantity.of(RationalScalar.of(1, 1), "s"), levels);
+    File folder = HomeDirectory.Pictures(hermiteArray.getClass().getSimpleName(), String.format("cx_%1d", levels));
+    folder.mkdirs();
+    Tensor matrix = hermiteArray.getMatrix();
+    HermiteArray.export(new File(folder, "id"), matrix);
+    HermiteArray.export(new File(folder, "ln"), matrix.map(RealScalar.ONE::add).map(Log.FUNCTION));
+  }
+}
--- src/main/java/ch/ethz/idsc/sophus/app/hs/HermiteDataExport.java
+++ src/main/java/ch/ethz/idsc/sophus/app/hs/HermiteDataExport.java
@@ -1,5 +1,5 @@
 // code by jph
-package ch.ethz.idsc.sophus.app.jph;
+package ch.ethz.idsc.sophus.app.hs;
 
 import java.io.File;
 import java.io.IOException;
